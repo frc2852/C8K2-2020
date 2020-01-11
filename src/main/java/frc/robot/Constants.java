0bafddfb522045e29493921893cde21b09bd4143
krystianBooker <krystian.booker@gmail.com>
2020-01-11 15:52:04 -0500

    Added constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,12 +8,53 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
+    // #region Controllers
+
+    // Controllers Mapping
+    public static final int DRIVER_CONTROLLER = 0;
+    public static final int OPERATOR_CONTROLLER = 1;
+
+    // Controller Button Mapping
+    public static final int A_BUTTON = 1;
+    public static final int B_BUTTON = 2;
+    public static final int X_BUTTON = 3;
+    public static final int Y_BUTTON = 4;
+    public static final int LEFT_BUMPER = 5;
+    public static final int RIGHT_BUMPER = 6;
+    public static final int BACK_BUTTON = 7;
+    public static final int START_BUTTON = 8;
+
+    // #endregion Controllers
+
+    // #region CAN Bus
+
+    // FRC Devices
+    public static final int PDP = 0;
+    public static final int PCM = 5;
+
+    // Drive
+    public static final int DRIVE_LEFT_MASTER = 1;
+    public static final int DRIVE_LEFT_SLAVE = 2;
+
+    public static final int DRIVE_RIGHT_MASTER = 3;
+    public static final int DRIVE_RIGHT_SLAVE = 4;
+
+    // #endregion CAN Bus
+
+    // #region PCM Bus
+
+    public static final int DRIVE_GEAR_BOX_OPEN = 0;
+    public static final int DRIVE_GEAR_BOX_CLOSE = 1;
+
+    // #endregion PCM Bus
 }
