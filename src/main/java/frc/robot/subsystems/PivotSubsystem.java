d339abb0e1bc9b06e678884eeab8a2b2f36b2c7c
Owen Mondoux <omondoux2003@gmail.com>
2020-02-08 15:47:19 -0500

    pivot finished

--- src/main/java/frc/robot/subsystems/PivotSubsystem.java
+++ src/main/java/frc/robot/subsystems/PivotSubsystem.java
@@ -8,22 +8,44 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class PivotSubsystem extends SubsystemBase {
 
-  private final CANSparkMax pivotMotorLeft = new CANSparkMax(Constants.)
+  private final CANSparkMax pivotMotorLeft = new CANSparkMax(Constants.PIVOT_LEFT, MotorType.kBrushless);
+  private final CANSparkMax pivotMotorRight = new CANSparkMax(Constants.PIVOT_RIGHT, MotorType.kBrushless);
   /**
    * Creates a new PivotSubsystem.
    */
   public PivotSubsystem() {
+
+    pivotMotorLeft.restoreFactoryDefaults();
+    pivotMotorRight.restoreFactoryDefaults();
+
+    pivotMotorLeft.setInverted(false);
+    pivotMotorRight.setInverted(true);
+
+    pivotMotorRight.setIdleMode(IdleMode.kBrake);
+    pivotMotorLeft.setIdleMode(IdleMode.kBrake);
 
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public void manualPivotUp(){
+    pivotMotorLeft.set(0.5);
+    pivotMotorRight.set(0.5);
+  }
+
+  public void manualPivotDown(){
+    pivotMotorLeft.set(-0.5);
+    pivotMotorRight.set(-0.5);
   }
 }
