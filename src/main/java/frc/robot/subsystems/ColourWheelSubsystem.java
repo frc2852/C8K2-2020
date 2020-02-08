dc7a415efbd518fb7b9e8c74662032289bf46551
Owen Mondoux <omondoux2003@gmail.com>
2020-02-08 15:46:30 -0500

    Create ColourWheelSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/ColourWheelSubsystem.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class ColourWheelSubsystem extends SubsystemBase {
+	private final TalonSRX colourWheelMotor = new TalonSRX(Constants.PI_WHEEL);
+  	/**
+  	 * Creates a new ColourWheelSubsystem.
+  	 */
+  	public ColourWheelSubsystem() {
+		colourWheelMotor.configFactoryDefault();
+
+		colourWheelMotor.setNeutralMode(NeutralMode.Brake);
+
+  	}
+
+  	@Override
+  	public void periodic() {
+  		// This method will be called once per scheduler run
+  	}
+}
