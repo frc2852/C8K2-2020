12901fad7bf8371538ac875f93ae4f7027887bba
Owen Mondoux <omondoux2003@gmail.com>
2020-02-10 18:12:57 -0500

    Update IntakeSubsystem.java

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -15,27 +15,28 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class IntakeSubsystem extends SubsystemBase {
-  private final VictorSPX intakeMotor = new VictorSPX(Constants.INTAKE);
-  /**
-   * Creates a new IntakeSubsystem.
-   */
-  public IntakeSubsystem() {
-    intakeMotor.configFactoryDefault();
+	private final VictorSPX intakeMotor = new VictorSPX(Constants.INTAKE);
 
-    intakeMotor.setNeutralMode(NeutralMode.Coast);
+	/**
+	 * Creates a new IntakeSubsystem.
+	 */
+	public IntakeSubsystem() {
+		intakeMotor.configFactoryDefault();
 
-  }
+		intakeMotor.setNeutralMode(NeutralMode.Coast);
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+	}
 
-  public void intakeForward(){
-    intakeMotor.set(ControlMode.Velocity, 1);
-  }
+	@Override
+	public void periodic() {
+		// This method will be called once per scheduler run
+	}
 
-  public void intakeReverse(){
-    intakeMotor.set(ControlMode.Velocity, -1);
-  }
+	public void intakeForward() {
+		intakeMotor.set(ControlMode.Velocity, 1);
+	}
+
+	public void intakeReverse() {
+		intakeMotor.set(ControlMode.Velocity, -1);
+	}
 }
