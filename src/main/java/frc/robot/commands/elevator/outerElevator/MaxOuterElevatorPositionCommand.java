8bd1481a2b4d5936f5565a86761b674f85dcb804
Owen Mondoux <omondoux2003@gmail.com>
2020-02-10 18:10:44 -0500

    Update MaxOuterElevatorPositionCommand.java

--- src/main/java/frc/robot/commands/elevator/outerElevator/MaxOuterElevatorPositionCommand.java
+++ src/main/java/frc/robot/commands/elevator/outerElevator/MaxOuterElevatorPositionCommand.java
@@ -11,34 +11,35 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.elevator.OuterElevatorSubsystem;
 
 public class MaxOuterElevatorPositionCommand extends CommandBase {
-  private OuterElevatorSubsystem outerElevatorSubsystem;
-  /**
-   * Creates a new MaxOuterElevatorPositionCommand.
-   */
-  public MaxOuterElevatorPositionCommand(OuterElevatorSubsystem _outerElevatorSubsystem) {
-    outerElevatorSubsystem = _outerElevatorSubsystem;
-    addRequirements(outerElevatorSubsystem);
-  }
+	private OuterElevatorSubsystem outerElevatorSubsystem;
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-  }
+	/**
+	 * Creates a new MaxOuterElevatorPositionCommand.
+	 */
+	public MaxOuterElevatorPositionCommand(OuterElevatorSubsystem _outerElevatorSubsystem) {
+		outerElevatorSubsystem = _outerElevatorSubsystem;
+		addRequirements(outerElevatorSubsystem);
+	}
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    outerElevatorSubsystem.outerElevatorMaxPosition();
-  }
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {
+	}
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {
+		outerElevatorSubsystem.outerElevatorMaxPosition();
+	}
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {
+	}
+
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		return false;
+	}
 }
