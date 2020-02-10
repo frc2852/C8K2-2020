07d097cf5711de5e688af647ebc18a25b8afbfcb
Owen Mondoux <omondoux2003@gmail.com>
2020-02-10 18:10:57 -0500

    Update IntakeForwardCommand.java

--- src/main/java/frc/robot/commands/intake/IntakeForwardCommand.java
+++ src/main/java/frc/robot/commands/intake/IntakeForwardCommand.java
@@ -11,34 +11,35 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.IntakeSubsystem;
 
 public class IntakeForwardCommand extends CommandBase {
-  private IntakeSubsystem intakeSubsystem;
-  /**
-   * Creates a new IntakeForwardCommand.
-   */
-  public IntakeForwardCommand(IntakeSubsystem _intakeSubsystem) {
-    intakeSubsystem = _intakeSubsystem;
-    addRequirements(intakeSubsystem);
-  }
+	private IntakeSubsystem intakeSubsystem;
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-  }
+	/**
+	 * Creates a new IntakeForwardCommand.
+	 */
+	public IntakeForwardCommand(IntakeSubsystem _intakeSubsystem) {
+		intakeSubsystem = _intakeSubsystem;
+		addRequirements(intakeSubsystem);
+	}
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    intakeSubsystem.intakeForward();
-  }
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {
+	}
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {
+		intakeSubsystem.intakeForward();
+	}
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {
+	}
+
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		return false;
+	}
 }
