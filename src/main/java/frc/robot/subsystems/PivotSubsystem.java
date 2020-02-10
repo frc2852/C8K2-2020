49a867c9af94b56725fd264627ba8c9b64a167de
Owen Mondoux <omondoux2003@gmail.com>
2020-02-10 18:13:24 -0500

    Update PivotSubsystem.java

--- src/main/java/frc/robot/subsystems/PivotSubsystem.java
+++ src/main/java/frc/robot/subsystems/PivotSubsystem.java
@@ -16,35 +16,36 @@ import frc.robot.Constants;
 
 public class PivotSubsystem extends SubsystemBase {
 
-  private final CANSparkMax pivotMotorLeft = new CANSparkMax(Constants.PIVOT_LEFT, MotorType.kBrushless);
-  private final CANSparkMax pivotMotorRight = new CANSparkMax(Constants.PIVOT_RIGHT, MotorType.kBrushless);
-  /**
-   * Creates a new PivotSubsystem.
-   */
-  public PivotSubsystem() {
+	private final CANSparkMax pivotMotorLeft = new CANSparkMax(Constants.PIVOT_LEFT, MotorType.kBrushless);
+	private final CANSparkMax pivotMotorRight = new CANSparkMax(Constants.PIVOT_RIGHT, MotorType.kBrushless);
 
-    pivotMotorLeft.restoreFactoryDefaults();
-    pivotMotorRight.restoreFactoryDefaults();
+	/**
+	 * Creates a new PivotSubsystem.
+	 */
+	public PivotSubsystem() {
 
-    pivotMotorRight.setInverted(true);
+		pivotMotorLeft.restoreFactoryDefaults();
+		pivotMotorRight.restoreFactoryDefaults();
 
-    pivotMotorRight.setIdleMode(IdleMode.kBrake);
-    pivotMotorLeft.setIdleMode(IdleMode.kBrake);
+		pivotMotorRight.setInverted(true);
 
-  }
+		pivotMotorRight.setIdleMode(IdleMode.kBrake);
+		pivotMotorLeft.setIdleMode(IdleMode.kBrake);
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+	}
 
-  public void manualPivotUp(){
-    pivotMotorLeft.set(0.5);
-    pivotMotorRight.set(0.5);
-  }
+	@Override
+	public void periodic() {
+		// This method will be called once per scheduler run
+	}
 
-  public void manualPivotDown(){
-    pivotMotorLeft.set(-0.5);
-    pivotMotorRight.set(-0.5);
-  }
+	public void manualPivotUp() {
+		pivotMotorLeft.set(0.5);
+		pivotMotorRight.set(0.5);
+	}
+
+	public void manualPivotDown() {
+		pivotMotorLeft.set(-0.5);
+		pivotMotorRight.set(-0.5);
+	}
 }
