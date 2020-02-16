6cab3b02fdf2014737622efec2b3515195e308e7
Owen Mondoux <omondoux2003@gmail.com>
2020-02-16 12:31:04 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,34 +10,38 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
 import edu.wpi.first.wpilibj2.command.button.Button;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.commands.commandGroups.LoadShootFromTrenchCommandGroup;
+import frc.robot.commands.command_groups.LoadShootFromColourWheelCommandGroup;
+import frc.robot.commands.command_groups.LoadShootFromTrenchCommandGroup;
 import frc.robot.commands.drive.DriveHighGearboxCommand;
 import frc.robot.commands.drive.DriveLowGearboxCommand;
 import frc.robot.commands.drive.DrivetrainCommand;
-import frc.robot.commands.elevator.innerElevator.MaxInnerElevatorPositionCommand;
-import frc.robot.commands.elevator.innerElevator.MinInnerElevatorPositionCommand;
-import frc.robot.commands.elevator.outerElevator.MaxOuterElevatorPositionCommand;
-import frc.robot.commands.elevator.outerElevator.MinOuterElevatorPositionCommand;
+import frc.robot.commands.elevator.MaxElevatorPositionCommand;
+import frc.robot.commands.elevator.MinElevatorPositionCommand;
 import frc.robot.commands.intake.IntakeForwardCommand;
 import frc.robot.commands.intake.IntakeReverseCommand;
+import frc.robot.commands.level.AutoLevelManualLeftCommand;
+import frc.robot.commands.level.AutoLevelManualRightCommand;
 import frc.robot.commands.magazine.ManualLoadCommand;
 import frc.robot.commands.magazine.ManualReverseLoadCommand;
+import frc.robot.commands.magazine.StopMagazineCommand;
 import frc.robot.commands.pivot.PivotClimbCommand;
 import frc.robot.commands.pivot.PivotColourWheelCommand;
 import frc.robot.commands.pivot.PivotPickUpCommand;
 import frc.robot.commands.pivot.PivotTrenchCommand;
 import frc.robot.commands.shooter.ShootFromColourWheelCommand;
 import frc.robot.commands.shooter.ShootFromTrenchCommand;
+import frc.robot.commands.shooter.StopShooterCommand;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.MagazineSubsystem;
 import frc.robot.subsystems.PivotSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.drive.DrivetrainSubsystem;
 import frc.robot.subsystems.drive.GearboxSubsystem;
-import frc.robot.subsystems.elevator.InnerElevatorSubsystem;
-import frc.robot.subsystems.elevator.OuterElevatorSubsystem;
+import frc.robot.subsystems.elevator.AutoLevelSubsystem;
+import frc.robot.subsystems.elevator.ElevatorSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -53,6 +57,7 @@ public class RobotContainer {
 	private XboxController DriverController = new XboxController(Constants.DRIVER_CONTROLLER);
 	private XboxController OperatorController = new XboxController(Constants.OPERATOR_CONTROLLER);
 
+	// Driver Buttons
 	private Button DriveButtonX = new JoystickButton(DriverController, Constants.X_BUTTON);
 	private Button DriveButtonA = new JoystickButton(DriverController, Constants.A_BUTTON);
 	private Button DriveButtonB = new JoystickButton(DriverController, Constants.B_BUTTON);
@@ -63,9 +68,18 @@ public class RobotContainer {
 	private Button DriveButtonStart = new JoystickButton(DriverController, Constants.START_BUTTON);
 	private Button DriveButtonRightJoystick = new JoystickButton(DriverController, Constants.RIGHT_JOYSTICK_BUTTON);
 	private Button DriveButtonLeftJoystick = new JoystickButton(DriverController, Constants.LEFT_JOYSTICK_BUTTON);
+
+	// Driver D-Pad
+	private POVButton DriveDpadUp = new POVButton(DriverController, Constants.PovUp);
+	private POVButton DriveDpadRight = new POVButton(DriverController, Constants.PovRight);
+	private POVButton DriveDpadDown = new POVButton(DriverController, Constants.PovDown);
+	private POVButton DriveDpadLeft = new POVButton(DriverController, Constants.PovLeft);	
+	
+	// Driver Triggers
 	private Trigger DriveButtonLeftTrigger = new JoystickButton(DriverController, Constants.LEFT_TRIGGER);
 	private Trigger DriveButtonRightTrigger = new JoystickButton(DriverController, Constants.RIGHT_TRIGGER);	
 
+	// Operator Buttons
 	private Button OperatorButtonX = new JoystickButton(OperatorController, Constants.X_BUTTON);
 	private Button OperatorButtonA = new JoystickButton(OperatorController, Constants.A_BUTTON);
 	private Button OperatorButtonB = new JoystickButton(OperatorController, Constants.B_BUTTON);
@@ -76,9 +90,16 @@ public class RobotContainer {
 	private Button OperatorButtonStart = new JoystickButton(OperatorController, Constants.START_BUTTON);
 	private Button OperatorButtonRightJoystick = new JoystickButton(DriverController, Constants.RIGHT_JOYSTICK_BUTTON);
 	private Button OperatorButtonLeftJoystick = new JoystickButton(DriverController, Constants.LEFT_JOYSTICK_BUTTON);
+
+	// Operator D-Pad
+	private POVButton OperatorDpadUp = new POVButton(DriverController, Constants.PovUp);
+	private POVButton OperatorDpadRight = new POVButton(DriverController, Constants.PovRight);
+	private POVButton OperatorDpadDown = new POVButton(DriverController, Constants.PovDown);
+	private POVButton OperatorDpadLeft = new POVButton(DriverController, Constants.PovLeft);	
+
+	// Operator Triggers
 	private Trigger OperatorButtonLeftTrigger = new JoystickButton(OperatorController, Constants.LEFT_TRIGGER);
 	private Trigger OperatorButtonRightTrigger = new JoystickButton(OperatorController, Constants.RIGHT_TRIGGER);
-
 
 	private final DrivetrainSubsystem drivetrainSubsystem = new DrivetrainSubsystem();
 
@@ -89,6 +110,7 @@ public class RobotContainer {
 	private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
 	private final ShootFromColourWheelCommand shootFromColourWheelCommand = new ShootFromColourWheelCommand(shooterSubsystem);
 	private final ShootFromTrenchCommand shootFromTrenchCommand = new ShootFromTrenchCommand(shooterSubsystem);
+	private final StopShooterCommand stopShooterCommand = new StopShooterCommand(shooterSubsystem);
 
 	private final PivotSubsystem pivotSubsystem = new PivotSubsystem();
 	private final PivotClimbCommand pivotClimbCommand = new PivotClimbCommand(pivotSubsystem);
@@ -100,20 +122,22 @@ public class RobotContainer {
 	private final IntakeForwardCommand intakeForwardCommand = new IntakeForwardCommand(intakeSubsystem);
 	private final IntakeReverseCommand intakeReverseCommand = new IntakeReverseCommand(intakeSubsystem);
 
-	private final InnerElevatorSubsystem innerElevatorSubsystem = new InnerElevatorSubsystem();
-	private final MaxInnerElevatorPositionCommand maxInnerElevatorPositionCommand = new MaxInnerElevatorPositionCommand(innerElevatorSubsystem);
-	private final MinInnerElevatorPositionCommand minInnerElevatorPositionCommand = new MinInnerElevatorPositionCommand(innerElevatorSubsystem);
-
-	private final OuterElevatorSubsystem outerElevatorSubsystem = new OuterElevatorSubsystem();
-	private final MaxOuterElevatorPositionCommand maxOuterElevatorPositionCommand = new MaxOuterElevatorPositionCommand(outerElevatorSubsystem);
-	private final MinOuterElevatorPositionCommand minOuterElevatorPositionCommand = new MinOuterElevatorPositionCommand(outerElevatorSubsystem);
+	private final ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
+	private final MaxElevatorPositionCommand MaxElevatorPositionCommand = new MaxElevatorPositionCommand(elevatorSubsystem);
+	private final MinElevatorPositionCommand MinElevatorPositionCommand = new MinElevatorPositionCommand(elevatorSubsystem);
 
 	private final MagazineSubsystem magazineSubsystem = new MagazineSubsystem();
 	private final ManualLoadCommand manualLoadCommand = new ManualLoadCommand(magazineSubsystem);
 	private final ManualReverseLoadCommand manualReverseLoadCommand = new ManualReverseLoadCommand(magazineSubsystem);
+	private final StopMagazineCommand stopMagazineCommand = new StopMagazineCommand(magazineSubsystem);
+
+	private final AutoLevelSubsystem autoLevelSubsystem = new AutoLevelSubsystem();
+	private final AutoLevelManualLeftCommand autoLevelManualLeftCommand = new AutoLevelManualLeftCommand(autoLevelSubsystem);
+	private final AutoLevelManualRightCommand autoLevelManualRightCommand = new AutoLevelManualRightCommand(autoLevelSubsystem);
 
 	// Command Groups
 	private final LoadShootFromTrenchCommandGroup loadShootFromTrenchCommandGroup = new LoadShootFromTrenchCommandGroup(shootFromTrenchCommand, manualLoadCommand);
+	private final LoadShootFromColourWheelCommandGroup loadShootFromColourWheelCommandGroup = new LoadShootFromColourWheelCommandGroup(manualLoadCommand, shootFromColourWheelCommand);
 	
 
 	/**
@@ -123,6 +147,8 @@ public class RobotContainer {
 		// Configure the button bindings
 		configureButtonBindings();
 		gearboxSubsystem.setDefaultCommand(driveLowGearboxCommand);
+		shooterSubsystem.setDefaultCommand(stopShooterCommand);
+		magazineSubsystem.setDefaultCommand(stopMagazineCommand);
 
 	}
 
@@ -140,16 +166,20 @@ public class RobotContainer {
 
 		DriveButtonLeftJoystick.toggleWhenPressed(driveHighGearboxCommand);
 
-		DriveButtonRightBumper.whenPressed(shootFromColourWheelCommand);
-		DriveButtonLeftBumper.whenPressed(loadShootFromTrenchCommandGroup);
+		// DriveButtonRightBumper.whenPressed(loadShootFromColourWheelCommandGroup);
+		// DriveButtonLeftBumper.whenPressed(loadShootFromTrenchCommandGroup);
 
-		// DriveButtonRightTrigger.whenActive(shooterFullSpeedCommand);
+		DriveButtonRightBumper.toggleWhenPressed(shootFromTrenchCommand);
+		DriveButtonLeftBumper.toggleWhenPressed(shootFromColourWheelCommand);
 
 		DriveButtonA.whenPressed(intakeForwardCommand);
 		DriveButtonY.whenPressed(intakeReverseCommand);
 
-		DriveButtonB.whenPressed(manualLoadCommand);
-		DriveButtonX.whenPressed(manualReverseLoadCommand);
+		DriveButtonB.toggleWhenPressed(manualLoadCommand);
+		DriveButtonX.toggleWhenPressed(manualReverseLoadCommand);
+
+		DriveDpadRight.whenPressed(autoLevelManualRightCommand);
+		DriveDpadLeft.whenPressed(autoLevelManualRightCommand);
 
 
 		// Operator Stick
