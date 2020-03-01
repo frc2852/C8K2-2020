aa8c552d03dbb2004b038bdcec2e587913da0ca6
David-Hockey <dahockey3@gmail.com>
2020-03-01 15:22:29 -0500

    Create IntakePivotLowCommand.java

---
+++ src/main/java/frc/robot/commands/intake/IntakePivotLowCommand.java
@@ -0,0 +1,44 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.intake.IntakePivotSubsystem;
+
+public class IntakePivotLowCommand extends CommandBase {
+  private final IntakePivotSubsystem intakePivotSubsystem;
+  /**
+   * Creates a new IntakePivotLowCommand.
+   */
+  public IntakePivotLowCommand(IntakePivotSubsystem _intakePivotSubsystem) {
+    intakePivotSubsystem = _intakePivotSubsystem;
+    addRequirements(intakePivotSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    intakePivotSubsystem.intakePivotPickUp();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
