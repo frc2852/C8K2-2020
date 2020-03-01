30a052b380f2d08febf60d7baa7cec61df09c735
David-Hockey <dahockey3@gmail.com>
2020-03-01 15:24:24 -0500

    Create IntakePivotSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/intake/IntakePivotSubsystem.java
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems.intake;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class IntakePivotSubsystem extends SubsystemBase {
+
+	private DoubleSolenoid intakePivotSolenoid = new DoubleSolenoid(Constants.INTAKE_UP, Constants.INTAKE_DOWN);
+
+	public IntakePivotSubsystem() {
+
+	}
+
+	@Override
+	public void periodic() {
+		// This method will be called once per scheduler run
+	}
+
+	public void intakePivotStartingConfig() {
+		intakePivotSolenoid.set(DoubleSolenoid.Value.kReverse);
+	}
+
+	public void intakePivotPickUp() {
+		intakePivotSolenoid.set(DoubleSolenoid.Value.kForward);
+	}
+}
